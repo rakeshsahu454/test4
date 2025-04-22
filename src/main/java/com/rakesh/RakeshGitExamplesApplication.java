d1df4540927c09dbc9b79992d5d56388089d0130
Rakeshgit config --global user.email rakeshkumarsahu36843@gmail.com <rakeshkumarsahu36843@gmail.com>
2025-04-22 10:43:21 +0530

    Fist Commit

---
+++ src/main/java/com/rakesh/RakeshGitExamplesApplication.java
@@ -0,0 +1,13 @@
+package com.rakesh;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RakeshGitExamplesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RakeshGitExamplesApplication.class, args);
+	}
+
+}
