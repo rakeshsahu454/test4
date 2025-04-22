bcb4f37d3f30330b555c8988f640a8a3aed1aab4
rakeshsahu454 <rakeshkumarsahu36843@gmail.com>
2025-04-22 10:52:43 +0530

    Update RakeshGitExamplesApplication.java

--- src/main/java/com/rakesh/RakeshGitExamplesApplication.java
+++ src/main/java/com/rakesh/RakeshGitExamplesApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class RakeshGitExamplesApplication {
 
 	public static void main(String[] args) {
+		int x = 100;
 		SpringApplication.run(RakeshGitExamplesApplication.class, args);
 	}
 
